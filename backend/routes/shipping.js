8f61e7a45c5dd61a678847dc21873fabca143d1e
Sivani Mohan <sivanimohanvv@gmail.com>
2025-11-09 19:55:39 +0530

    added mobile compatible req

--- backend/routes/shipping.js
+++ backend/routes/shipping.js
@@ -6,17 +6,3 @@ const { getShippingOptions } = require('../controllers/shippingController');
 router.get('/', getShippingOptions);
 
 module.exports = router;
-const express = require('express');
-const router = express.Router();
-
-// Simple shipping endpoint returning a free shipping option
-// GET /api/shipping
-router.get('/', (req, res) => {
-  res.json({
-    options: [
-      { name: 'Standard Shipping', cost: 0, description: 'Free' }
-    ]
-  });
-});
-
-module.exports = router;
